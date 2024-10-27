4b7f3802b66d8a01c41131d955b29c5653f8adfe
Pavlo Myrskyi <pavlo.myrskyi@games>
2024-10-27 15:36:31 +0100

    updated cors

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-// index.js
 const express = require('express');
 const cors = require('cors'); // Import CORS package
 const app = express();
 const progressRoutes = require('./routes/progress');
 const errorHandler = require('./middlewares/errorHandler');
 
-// Configure CORS to allow requests from http://localhost:3000
+// List of allowed origins
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://git-app-alpha.vercel.app'
+];
+
+// Configure CORS options dynamically
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200
 };
-app.use(cors(corsOptions));
+
+app.use(cors(corsOptions)); // Apply CORS with updated options
 
 app.use(express.json()); // for parsing application/json
 app.use('/progress', progressRoutes); // Load routes
