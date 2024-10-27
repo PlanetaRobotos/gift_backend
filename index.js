4e50dcfc6b8fe6516dad2f211642a0c72e6a1e38
Pavlo Myrskyi <pavlo.myrskyi@games>
2024-10-27 14:14:53 +0100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // index.js
 const express = require('express');
+const cors = require('cors'); // Import CORS package
 const app = express();
 const progressRoutes = require('./routes/progress');
 const errorHandler = require('./middlewares/errorHandler');
 
-app.use(cors());
+// Configure CORS to allow requests from http://localhost:3000
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 app.use(express.json()); // for parsing application/json
 app.use('/progress', progressRoutes); // Load routes
 app.use(errorHandler); // Error handling middleware
